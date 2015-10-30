619a44e675c085172ff245b7b8ec1210fe3ed84d
Deadalus <Deadalusmask@gmail.com>
2015-10-30 22:21:59 +0800

    登录
    及激活后事件表单创建

--- active.php
+++ active.php
@@ -7,16 +7,24 @@ $nowtime = time();
 $query = mysql_query("select id,token_exptime from t_user where status='0' and `token`='$verify'");
 $row = mysql_fetch_array($query);
 if($row){
-	if($nowtime>$row['token_exptime']){ //30min
+	if($nowtime>$row['token_exptime']){
 		$msg = '您的激活有效期已过.';
 	}else{
 		mysql_query("update t_user set status=1 where id=".$row['id']);
 		if(mysql_affected_rows($link)!=1) die(0);
-		$msg = '激活成功！';
+		$msg = '激活成功！'
+		$tablename=mysql_query("select username from where id=".$row['id']);
+		mysql_query("CREATE TABLE [$tablename]
+						('id' int(10) NOT NULL AUTOINCRMENT,
+						 'date' int(10) NOT NULL COMMENT '事件日期',
+						 'location' VARCHAR (50) NOT NULL COMMENT '事件地点',
+						 'event' VARCHAR(50) NOT NULL COMMENT '事件名',
+						 'describe' VARCHAR (200) NOT NULL COMMENT '事件描述',
+						 PRIMARY KEY ('id')
+						)ENGINE=MyISAM DEFAULT CHARSET=utf8");
 	}
 }else{
-	$msg = 'error.';	
+	$msg = 'error.';
 }
-
 echo $msg;
 ?>
---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+include_once("connect.php");
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$username=test_input($_POST['username']);
+$password=md5(test_input($_POST['password']));
+$query = mysql_query("select id from t_user where username='$username'");
+$num = mysql_num_rows($query);
+if($num==1) {
+    $que = mysql_query("select password from t_user where id=='$query'");
+    if ($password==$que) {
+        echo '登陆成功';
+        //这里写推送信息
+    }
+    else echo '密码错误 请重试';
+}
+else echo '用户名不存在';
